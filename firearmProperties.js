69994b1fb71bfbfd392f02e5f12cf92e031d5815
Graham <gquinlan@uoguelph.ca>
2024-10-01 15:59:47 -0400

    added stuff

---
+++ firearmProperties.js
@@ -0,0 +1,6 @@
+// Adds new Weapon types of Hand Cannon and Magnum
+Hooks.once("init", () => {
+    CONFIG.DND5E.weaponIds.handCannon = "uuid";
+    CONFIG.DND5E.weaponIds.rifle = "uuid";
+    // etc etc
+  });
